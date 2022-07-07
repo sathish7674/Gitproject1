48617c4b0338e37d2344b5883e98619b3a4da3af
sathish7674 <orsusathish1997@gmail.com>
2022-07-07 17:26:10 +0530

    commit the above code

---
+++ spring2/src/spring2/Spring.java
@@ -0,0 +1,14 @@
+package spring2;
+
+public class Spring {
+
+	public Spring() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
